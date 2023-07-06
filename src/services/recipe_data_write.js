c8aa58b7fcb6554e234756fee778769ff4f9c572
tkbessler <tkbessler@gmail.com>
2023-07-06 09:10:20 -0500

    add recipe post request

--- src/services/recipe_data_write.js
+++ src/services/recipe_data_write.js
@@ -1,27 +1,19 @@
 import { sheetURL } from '../constants';
 
 export const writeRecipeDataToCSV = async (recipeData) => {
-  console.log(recipeData);
-  // try {
-  //   const response = await fetch(sheetURL, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'text/csv',
-  //     },
-  //     body: csvData,
-  //   });
-  //   if (response.ok) {
-  //     console.log('Recipe data has been written to the CSV file successfully.');
-  //   } else {
-  //     throw new Error('Failed to write recipe data to the CSV file.');
-  //   }
-  // } catch (error) {
-  //   console.error('Error occurred while writing recipe data to the CSV file:', error);
-  // }
-};
-
-const convertToCSV = (data) => {
-  const headers = Object.keys(data[0]).join(',');
-  const rows = data.map((item) => Object.values(item).join(','));
-  return `${headers}\n${rows.join('\n')}`;
+  await fetch(sheetURL, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(recipeData)
+  })
+  .then((r) => r.json())
+  .then((data) => {
+    console.log("Successful Post");
+  })
+  .catch((error) => {
+    console.error("An error occurred: ", error);
+  });
 };
