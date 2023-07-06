b3c0e2daf082451fe5851f67e34cccfeec22b255
tkbessler <tkbessler@gmail.com>
2023-07-06 16:37:36 -0500

    request button and svg icons

--- src/containers/action_container.js
+++ src/containers/action_container.js
@@ -5,10 +5,11 @@ import { sheetUrl } from '../constants';
 export const ActionContainer = ({ addRecipeClick }) => {
   return (
     <div className="action-container">
-      <Button name='Shuffle' icon='shuffle_icon.png' onClick={onShuffle} tooltip='Shuffle Recipes' />
-      <Button name='Add Recipe' icon='add_icon.png' onClick={addRecipeClick} tooltip='Add New Recipe' />
-      <Button name='Settings' icon='settings_icon.png' onClick={onSettings} tooltip='Settings' />
-      <Button name='Link' icon='link_icon.png' onClick={onLink} tooltip='Go To Spreadsheet' />
+      <Button name='Shuffle' icon='shuffle_icon.svg' onClick={onShuffle} tooltip='Shuffle Recipes' />
+      <Button name='Add Recipe' icon='add_icon.svg' onClick={addRecipeClick} tooltip='Add New Recipe' />
+      <Button name='Settings' icon='settings_icon.svg' onClick={onSettings} tooltip='Settings' />
+      <Button name='Link' icon='link_icon.svg' onClick={onLink} tooltip='Go To Spreadsheet' />
+      <Button name='Request Meal' icon='request_icon.svg' onClick={onRequest} tooltip='Request Meal' />
     </div>
   );
 }
@@ -23,4 +24,8 @@ const onSettings = () => {
 
 const onLink = () => {
   window.open(sheetUrl, '_blank');
+}
+
+const onRequest = () => {
+  console.log("Request Meal");
 }
