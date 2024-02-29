d69b8724f78deb336560c261c19b856e21e68123
tkbessler <tkbessler@gmail.com>
2024-02-28 18:58:53 -0600

    backend working

--- client/src/services/recipe_data_fetch.js
+++ client/src/services/recipe_data_fetch.js
@@ -11,16 +11,7 @@
 // const doc = new google.sheets.GoogleSpreadsheet(process.env.REACT_APP_GOOGLE_SHEET_ID, serviceAccountAuth);
 
 export default async function getRecipeData() {
-  // const client = await auth.getClient();
-
-  // const sheets = await google.sheets({ version: 'v4', auth: client });
-
-  // const response = await sheets.spreadsheets.get({
-  //   auth,
-  //   spreadsheetId: process.env.REACT_APP_GOOGLE_SHEET_ID
-  // })
-
-  // console.log(response);
+  
   
   return [];
 }
--- server/routes/google.js
+++ server/routes/google.js
@@ -1,8 +1,28 @@
 var express = require("express");
 var router = express.Router();
+var { google } = require('googleapis');
+const dotenv = require('dotenv').config();
 
-router.get("/", function(req, res, next) {
-    res.send("API is working properly");
+router.get("/", async function(req, res, next) {
+    const auth = new google.auth.GoogleAuth({
+        keyFilename: '../server/google_credentials.json',
+        scopes: 'https://www.googleapis.com/auth/spreadsheets'
+    });
+
+    try {
+        const client = await auth.getClient();
+        const sheets = await google.sheets({ version: 'v4', auth: client });        
+        const rows = await sheets.spreadsheets.values.get({
+            auth,
+            spreadsheetId: process.env.GOOGLE_SHEET_ID,
+            range: "Sheet1!A:F"
+        });
+        res.send(rows.data.values);
+    } catch(e) {
+        console.info("[Google Sheet Service]", e.message);
+        res.send("{}");
+    }
+    
 });
 
 module.exports = router;
